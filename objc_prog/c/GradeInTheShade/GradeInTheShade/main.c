f840c191e950552ac9d8cf6a051922c60b72dd2c
Alex Kudryashov <alex.kudryashov@gmail.com>
2014-12-14 22:57:12 +0100

    Half of the Chapter 40

---
+++ objc_prog/c/GradeInTheShade/GradeInTheShade/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  GradeInTheShade
+//
+//  Created by Alexander Kudryashov on 14.12.2014.
+//  Copyright (c) 2014 Alexander Kudryashov. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float averageFloats(float data[], int dataCount) {
+    float sum = 0.0;
+    for (int i = 0; i < dataCount; i++) {
+        sum = sum + data[i];
+    }
+    
+    return sum / dataCount;
+}
+
+int main(int argc, const char * argv[]) {
+    float gradeBook[] = {60.2, 94.5, 81.1};
+    
+    float average = averageFloats(gradeBook, 3);
+    
+    printf("Average = %.2f\n", average);
+    
+    return 0;
+}
