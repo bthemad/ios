1ee57999dbfde8bc39cd25c2d65518dfd7744a78
Alex Kudryashov <alex.kudryashov@gmail.com>
2015-01-12 21:56:29 +0100

    Done with Chapter 1

--- ios_prog/Quiz/Quiz/QuizViewController.m
+++ ios_prog/Quiz/Quiz/QuizViewController.m
@@ -13,16 +13,44 @@
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
 @property (nonatomic, weak) IBOutlet UILabel *answerLabel;
 
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @end
 
 @implementation QuizViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+    if (self) {
+        self.questions = @[@"From what cognac is made?",
+                           @"What's 7 + 7?",
+                           @"What's the capital of Vermont"];
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    return self;
+}
+
 - (IBAction)showQuestion:(id)sender {
+    self.currentQuestionIndex++;
     
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    
+    self.answerLabel.text = @"???";
 }
 
 - (IBAction)showAnswer:(id)sender {
-    
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
 }
 
 - (void)viewDidLoad {
