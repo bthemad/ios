0ce7811ed90d13c1e5f7a30668a2c129cd519924
Alex Kudryashov <alex.kudryashov@gmail.com>
2015-02-09 23:43:16 +0100

    Chapter 6 with local notifications

---
+++ ios_prog/HypnoNerd/HypnoNerd/HypnosisViewController.m
@@ -0,0 +1,35 @@
+//
+//  HypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by Alexander Kudryashov on 09.02.2015.
+//  Copyright (c) 2015 Alexander Kudryashov. All rights reserved.
+//
+
+#import "HypnosisViewController.h"
+#import "HypnosisView.h"
+
+@interface HypnosisViewController ()
+
+@end
+
+@implementation HypnosisViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+    if (self) {
+        self.tabBarItem.title = @"Hypnotize";
+        UIImage *i = [UIImage imageNamed:@"Hypno.png"];
+        self.tabBarItem.image = i;
+    }
+
+    return self;
+}
+
+- (void)loadView {
+    HypnosisView *hypnosisView = [[HypnosisView alloc] init];
+    self.view = hypnosisView;
+}
+
+@end
